8ea96792bcc0da211b99a1844d01a174ee52a933
Todd W <117058912+ToddW317@users.noreply.github.com>
2023-11-07 16:27:51 -0800

    Update page.tsx

--- chatapp/app/page.tsx
+++ chatapp/app/page.tsx
@@ -26,7 +26,7 @@ export default function HomePage() {
                 Chat with Anyone, anywhere!
               </h1>
               <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300">
-                You speak your language, they speak their language.{" "}
+                You speak your language, they speak their language!{" "}
                 <span className="text-indigo-600 dark:text-indigo-500">
                   Let AI handle the translation.
                 </span>
